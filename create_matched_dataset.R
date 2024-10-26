24f813e75f16fefd59c33fd07da20b3081a1092d
ruvilonix <osmiumsky@tutanota.com>
2024-10-26 14:41:22 -0400

    draft of script to create dataset then match

--- create_matched_dataset.R
+++ create_matched_dataset.R
@@ -1,41 +1,90 @@
 library(dplyr)
 library(tidyr)
 
-MATCHING_COLUMNS <- c("sex", "bmi", "age", "wkld_D1_AT")
-
-# Define function to reshape participant data
-reshape_participant_data <- function(df, non_duplicate_cols = NULL) {
-  # Define the columns to be duplicated
-  duplicate_cols <- setdiff(names(df), c("ParticipantID", "Time_Point", "Study_Visit", non_duplicate_cols))
-  
-  # Pivot the data to a wide format
-  df_reshaped <- df %>%
-    pivot_wider(
-      id_cols = c(ParticipantID, all_of(non_duplicate_cols)),
-      names_from = c(Study_Visit, Time_Point),
-      values_from = all_of(duplicate_cols),
-      names_sep = "_"
-    )
-  
-  return(df_reshaped)
-}
-
-# Read in the data
-df <- read.delim("../cpet_clinical_data_v2.tsv", sep = "\t")
+COLUMNS_TO_MATCH <- c("sex", "bmi", "age", "D1_AT_wkld")
 
 # Specify non-duplicate columns
 non_duplicate_cols <- c("matched_pair", "sex", "phenotype", "race", "age", 
                         "height_in", "weight_lb", "bmi", "bas_score", 
                         "q_education", "q_reclined", "q_sleeprefreshing", 
-                        "q_hoursinbed")
+                        "q_hoursinbed", "test_site")
 
-# Reshape the data
-df_merged <- reshape_participant_data(df, non_duplicate_cols)
+# Read in the data
+df <- read.delim("../cpet_clinical_data_v2.tsv", sep = "\t")
 
-# Select necessary columns for propensity score analysis (assuming PROPENSITY_COLUMNS is defined)
-# Replace 'PROPENSITY_COLUMNS' with actual column names
-prop_data <- df_merged %>%
-  select(ParticipantID, phenotype, all_of(MATCHING_COLUMNS))
+ids_to_exclude <- c('PI-012', 'PI-043', 'PI-147', 'PI-008', 'PI-018', 'PI-029', 'PI-057', 'PI-082', 'PI-091', 'PI-128')
+df <- df[!(df$Time_Point == "max" & df$ParticipantID %in% ids_to_exclude), ]
+
+# First convert specified columns to numeric, coercing errors to NA, suppressing warnings
+df <- df %>%
+  mutate(across(all_of(setdiff(colnames(df), c('ParticipantID', 'Study_Visit', 'Time_Point', 'sex', 'phenotype'))), 
+                ~ suppressWarnings(as.numeric(as.character(.)))))
+
+# Convert these columns to factors
+df <- df %>%
+  mutate(across(all_of(c('Study_Visit', 'Time_Point', 'sex', 'phenotype')), 
+                ~ as.factor(.)))
+
+
+# Function to calculate differences between D2 and D1 for specified columns
+calculate_day_diffs <- function(df, diff_cols) {
+  # Convert specified columns to numeric, coercing errors to NA, suppressing warnings
+  df <- df %>%
+    mutate(across(all_of(diff_cols), ~suppressWarnings(as.numeric(as.character(.)))))
+  
+  # Step 1: Calculate differences
+  df_diffs <- df %>%
+    group_by(ParticipantID, Time_Point) %>%
+    arrange(ParticipantID, Time_Point, Study_Visit) %>%
+    summarise(
+      across(!all_of(c(diff_cols, "Study_Visit")), first),
+      across(all_of(diff_cols), 
+             list(
+               diff_abs = ~if_else(any(is.na(.)), NA_real_, last(.) - first(.)),
+               diff_pct = ~if_else(any(is.na(.)) | first(.) == 0, NA_real_, (last(.) - first(.))/first(.) * 100)
+             )
+      ),
+      .groups = "drop"
+    ) %>%
+    pivot_wider(
+      id_cols = !matches("_diff_(abs|pct)$"),
+      names_from = Time_Point,
+      values_from = matches("_diff_(abs|pct)$"),
+      names_glue = "{Time_Point}_{.value}"
+    )
+  
+  # Step 2: Pivot wider to get single day metrics (D1, D2) for specified columns
+  df_single <- df %>%
+    pivot_wider(
+      id_cols = ParticipantID,
+      names_from = c(Time_Point, Study_Visit),
+      values_from = all_of(diff_cols),
+      names_glue = "{Study_Visit}_{Time_Point}_{.value}"
+    )
+  
+  # Step 3: Merge single day metrics with the diffs
+  df_combined <- left_join(df_single, df_diffs, by = "ParticipantID")
+  
+  return(df_combined)
+}
+
+
+
+# Columns where we don't need to calculate difference between days
+non_diff_cols <- union(non_duplicate_cols, c("ParticipantID", "Study_Visit", "Time_Point"))
+
+# Columns where we are going to calculate difference between days
+diff_cols <- setdiff(colnames(df), non_diff_cols)
+
+# Run the difference calculation on the specified columns
+diff_df <- calculate_day_diffs(df, diff_cols)
+
+
+
+# Create dataframe only with columns for matching
+prop_data <- diff_df %>%
+  select(ParticipantID, phenotype, all_of(COLUMNS_TO_MATCH))
+
 
 # Display final data
 prop_data
