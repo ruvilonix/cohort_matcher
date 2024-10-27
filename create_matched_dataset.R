919243c7d09369117384898bae7b6c523f791183
ruvilonix <osmiumsky@tutanota.com>
2024-10-26 20:26:54 -0400

    script fully works

--- create_matched_dataset.R
+++ create_matched_dataset.R
@@ -1,6 +1,11 @@
 library(dplyr)
 library(tidyr)
 
+
+##################################
+##### Add difference columns #####
+##################################
+
 # Specify non-duplicate columns
 non_duplicate_cols <- c("matched_pair", "sex", "phenotype", "race", "age", 
                         "height_in", "weight_lb", "bmi", "bas_score", 
@@ -10,10 +15,11 @@ non_duplicate_cols <- c("matched_pair", "sex", "phenotype", "race", "age",
 # Read in the data
 df <- read.delim("../cpet_clinical_data_v2.tsv", sep = "\t")
 
+# Exclude participants that didn't meet criteria for max, only for max time point
 ids_to_exclude <- c('PI-012', 'PI-043', 'PI-147', 'PI-008', 'PI-018', 'PI-029', 'PI-057', 'PI-082', 'PI-091', 'PI-128')
 df <- df[!(df$Time_Point == "max" & df$ParticipantID %in% ids_to_exclude), ]
 
-# First convert specified columns to numeric, coercing errors to NA, suppressing warnings
+# Convert specified columns to numeric, coercing errors to NA, suppressing warnings
 df <- df %>%
   mutate(across(all_of(setdiff(colnames(df), c('ParticipantID', 'Study_Visit', 'Time_Point', 'sex', 'phenotype'))), 
                 ~ suppressWarnings(as.numeric(as.character(.)))))
@@ -26,9 +32,6 @@ df <- df %>%
 
 # Function to create a dataframe with differences between D2 and D1 for specified columns
 calculate_day_diffs <- function(df, diff_cols) {
-  # Convert specified columns to numeric, coercing errors to NA, suppressing warnings
-  df <- df %>%
-    mutate(across(all_of(diff_cols), ~suppressWarnings(as.numeric(as.character(.)))))
   
   # Step 1: Calculate differences
   df_diffs <- df %>%
@@ -74,9 +77,97 @@ non_diff_cols <- union(non_duplicate_cols, c("ParticipantID", "Study_Visit", "Ti
 # Columns where we are going to calculate difference between days
 diff_cols <- setdiff(colnames(df), non_diff_cols)
 
-# Run the difference calculation on the specified columns
+# Run the difference calculation on the specified columns and create the full cohort dataframe
 diff_df <- calculate_day_diffs(df, diff_cols)
 
 
 
+################################
+### Create matching pairs ######
+################################
 
+# Choose variables on which to pair participants with similar values
+COLUMNS_TO_MATCH <- c("sex", "bmi", "age", "D1_AT_wkld")
+
+# Create dataframe only with columns for matching
+prop_data <- diff_df %>%
+  select(ParticipantID, phenotype, all_of(COLUMNS_TO_MATCH))
+
+# Find matches. Set caliper values to max SD that rows in matched pairs can differ. 
+m.out3 <- matchit(phenotype ~ bmi + age + D1_AT_wkld,
+                  data = diff_df,
+                  method = "nearest", distance = "mahalanobis",
+                  exact = ~ sex,
+                  caliper = c(bmi = 1, age = 1, D1_AT_wkld = .5))
+
+# Display summary of matching
+summary(m.out3)
+
+# Plots to visualize matching
+plot(summary(m.out3))
+
+plot(m.out3, type = "density", interactive = FALSE,
+     which.xs = ~bmi + age + D1_AT_wkld)
+
+# Create dataframe with matched pairs
+m.data <- match.data(m.out3)
+
+# Remove weights column
+m.data <- subset(m.data, select = -weights)
+
+# Rename matched pair column
+colnames(m.data)[colnames(m.data) == 'subclass'] <- 'new_matched_pair'
+
+# Filter `diff_df` to include only rows where `ParticipantID` is in `m.data`
+full_matched <- diff_df[diff_df$ParticipantID %in% m.data$ParticipantID, ]
+
+# Add new_matched_pair column to final df to create final matched dataset
+full_matched <- merge(diff_df, m.data[c('ParticipantID', 'new_matched_pair')])
+
+
+#################################
+##### Statistical tests #########
+#################################
+
+
+only_diffs_df <- m.data %>% 
+  select(contains('diff'), 'ParticipantID', 'phenotype')
+
+check_distinct_values <- function(df, column_name) {
+  # Get unique non-NA values
+  distinct_values <- unique(na.omit(df[[column_name]]))
+  # Check if we have at least 2
+  length(distinct_values) >= 2
+}
+
+results <- data.frame(feature=character(),
+                      p_value=double(),
+                      stringsAsFactors = FALSE)
+
+for (col in setdiff(colnames(only_diffs_df), c('ParticipantID', 'phenotype'))) {
+  
+  # Skip if not enough distinct values
+  if (!check_distinct_values(only_diffs_df, col)) {
+    message(sprintf("Skipping %s - fewer than 2 distinct values", col))
+    next
+  }
+  
+  # Remove rows that have a null for the current column
+  nulls_removed <- only_diffs_df[complete.cases(only_diffs_df[col]), ]
+  
+  # Create the formula dynamically using as.formula()
+  formula <- as.formula(paste(col, "~ phenotype"))
+  
+  test_result <- wilcox.test(formula, data = nulls_removed)
+  result_df <- data.frame(feature=col,
+                       p_value=test_result$p.value,
+                       stringsAsFactors = FALSE)  
+  
+  results <- rbind(results, result_df)
+}
+
+# Add BH-corrected p-values as a new column
+results$p_adjusted <- p.adjust(results$p_value, method = "BH")
+
+# Sort by adjusted p-value
+results <- results[order(results$p_adjusted), ]
